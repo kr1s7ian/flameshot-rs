a94406ae85422df191ad8413e8eb471e4593f5c5
kr1s7ian <kristianboy44@gmail.com>
2023-03-20 15:46:45 +0100

    changed filename and struct names to match flameshot args

--- src/params/mod.rs
+++ src/params/mod.rs
@@ -1,6 +1,6 @@
-pub mod fullscreens;
+pub mod full;
 pub mod gui;
 pub mod screen;
-pub use crate::params::fullscreens::FullscreensArgs;
+pub use crate::params::full::FullArgs;
 pub use crate::params::gui::GuiArgs;
 pub use crate::params::screen::ScreenArgs;
